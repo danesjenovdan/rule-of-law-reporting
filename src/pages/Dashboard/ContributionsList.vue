1854cdbdef46d83a47989d04f9b4bf16a833f79b
patricijab <patricija.brecko@gmail.com>
2022-04-26 15:01:29 +0200

    contributions list styles and filters modal

--- src/pages/Dashboard/ContributionsList.vue
+++ src/pages/Dashboard/ContributionsList.vue
@@ -2,20 +2,30 @@
   <header>
     <SmallHeader />
     <PillButtonNav />
-    <h2>Seznam vseh prispevkov</h2>
-    <div style="margin: 1rem 0">TODO: filtri</div>
+    <div class="tools">
+      <span>{{ pageInfo.totalRows }} prispevkov</span>
+      <div class="filters">
+        <div class="filter" @click="showFiltersModal = true">
+          <div class="filter-icon"></div>
+          Filtiraj prikaz
+        </div>
+        <div class="search">
+          <div class="search-icon"></div>
+        </div>
+      </div>
+    </div>
   </header>
   <main>
     <div>
-      <div>
-        {{ pageInfo }}
-        <div
-          v-for="contribution in contributions"
-          :key="contribution.id"
-          style="font-size: 25px"
-        >
-          {{ contribution['Ime prispevka'] }}
-        </div>
+      <!-- TODO: redirect to single contribution view on click -->
+      <div
+        v-for="contribution in contributions"
+        :key="contribution.id"
+        class="contribution"
+        @click="$router.push({ name: 'new-contribution' })"
+      >
+        <span>{{ contribution['Ime prispevka'] }}</span>
+        <span class="arrow-right-icon"></span>
       </div>
     </div>
   </main>
@@ -29,22 +39,26 @@
       </FormKit>
     </div>
   </footer>
+  <FiltersModal v-if="showFiltersModal" @close="showFiltersModal = false" />
 </template>
 
 <script>
 import SmallHeader from '../../components/Header/SmallHeader.vue';
 import PillButtonNav from '../../components/PillButtonNav.vue';
+import FiltersModal from '../../components/FiltersModal.vue';
 import { getContributions } from '../../helpers/api.js';
 
 export default {
   components: {
     SmallHeader,
     PillButtonNav,
+    FiltersModal,
   },
   data() {
     return {
       contributions: [],
       pageInfo: {},
+      showFiltersModal: false,
     };
   },
   mounted() {
@@ -59,3 +73,62 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+@import '../../assets/scss/variables';
+
+.tools {
+  display: flex;
+  justify-content: space-between;
+  align-items: end;
+  margin: 1rem 0 0.75rem 0;
+  span {
+    font-size: 10px;
+    font-style: italic;
+    padding-bottom: 4px;
+  }
+}
+
+.filters {
+  display: flex;
+  align-items: center;
+  .filter {
+    display: flex;
+    align-items: center;
+    margin-right: 10px;
+    border-radius: 3px;
+    background-color: $color-orange;
+    padding: 4px 6px;
+    font-size: 10px;
+    .filter-icon {
+      width: 11px;
+      height: 13px;
+      margin-right: 5px;
+    }
+  }
+  .search {
+    border-radius: 3px;
+    background-color: $color-orange;
+    padding: 5px;
+    .search-icon {
+      width: 11px;
+      height: 13px;
+    }
+  }
+}
+
+.contribution {
+  font-size: 12px;
+  font-weight: 600;
+  padding: 20px 0;
+  border-bottom: 1px solid $color-accent-light;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.arrow-right-icon {
+  width: 8px;
+  height: 13px;
+}
+</style>
