d602e5239b988d2727b095d848d5691ee4ade0cd
patricijab <patricija.brecko@gmail.com>
2022-05-04 13:28:17 +0200

    update filters design and add filters to export contributions view

--- src/pages/Dashboard/ExportContributions.vue
+++ src/pages/Dashboard/ExportContributions.vue
@@ -1,10 +1,11 @@
 <template>
   <header>
     <SmallHeader />
-    <div class="tools">
-      <span>{{ pageInfo.totalRows }} prispevkov</span>
-      <span>Število izbranih: {{ checkboxData.length }}</span>
-    </div>
+    <ToolsBar
+      :contributionsNo="pageInfo.totalRows"
+      :chosenNo="checkboxData.length"
+      @close="showFiltersModal = true"
+    />
   </header>
   <main v-if="contributions.length > 0">
     <!-- TODO: remove formData, as it is not working properly -->
@@ -54,15 +55,20 @@
       <FormKit type="button"> Izvozi prispevke </FormKit>
     </div>
   </footer>
+  <FiltersModal v-if="showFiltersModal" @close="showFiltersModal = false" />
 </template>
 
 <script>
 import SmallHeader from '../../components/Header/SmallHeader.vue';
+import FiltersModal from '../../components/FiltersModal.vue';
+import ToolsBar from '../../components/Header/ToolsBar.vue';
 import { getContributions } from '../../helpers/api.js';
 
 export default {
   components: {
     SmallHeader,
+    FiltersModal,
+    ToolsBar,
   },
   data() {
     return {
@@ -99,56 +105,6 @@ export default {
 
 <style lang="scss" scoped>
 @import '../../assets/scss/variables';
-
-.tools {
-  display: flex;
-  justify-content: space-between;
-  align-items: end;
-  margin: 1rem 0 0.75rem 0;
-  span {
-    font-size: 10px;
-    font-style: italic;
-    padding-bottom: 4px;
-  }
-}
-
-.filters {
-  display: flex;
-  align-items: center;
-  .filter {
-    display: flex;
-    align-items: center;
-    margin-right: 10px;
-    border-radius: 3px;
-    background-color: $color-orange;
-    padding: 4px 6px;
-    font-size: 10px;
-    .filter-icon {
-      width: 11px;
-      height: 13px;
-      margin-right: 5px;
-    }
-  }
-  .search {
-    border-radius: 3px;
-    background-color: $color-orange;
-    padding: 5px;
-    .search-icon {
-      width: 11px;
-      height: 13px;
-    }
-  }
-}
-
-.contribution {
-  font-size: 12px;
-  font-weight: 600;
-  padding: 20px 0;
-  border-bottom: 1px solid $color-accent-light;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
 
 .arrow-right-icon {
   width: 8px;
