7c9dd79a55a3d7b17e681e386265773770e4ad82
patricijab <patricija.brecko@gmail.com>
2022-05-06 08:17:22 +0200

    connection dots between events

--- src/components/EventListElement.vue
+++ src/components/EventListElement.vue
@@ -28,6 +28,11 @@
         :source="source"
       />
     </div>
+    <div v-if="eventConnected" class="connection-dots">
+      <div class="dot"></div>
+      <div class="line"></div>
+      <div class="dot"></div>
+    </div>
   </div>
 </template>
 
@@ -50,6 +55,7 @@ export default {
   data() {
     return {
       sources: [],
+      eventConnected: false,
     };
   },
   async mounted() {
@@ -68,7 +74,8 @@ export default {
 .event {
   border: 1px solid $color-medium-grey;
   border-radius: 3px;
-  margin-bottom: 1rem;
+  margin-bottom: 1.5rem;
+  position: relative;
   .event-header {
     padding: 1.5rem;
   }
@@ -102,6 +109,27 @@ export default {
       color: $color-accent;
       margin-bottom: 0.5rem;
       padding: 0 1.5rem;
+    }
+  }
+  .connection-dots {
+    position: absolute;
+    left: 1.5rem;
+    top: -26px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .dot {
+      height: 12px;
+      width: 12px;
+      border: 3px solid $color-accent;
+      border-radius: 6px;
+      background-color: $bg-color;
+    }
+    .line {
+      height: 6px;
+      width: 3px;
+      background-color: $color-accent;
+      margin: 1px;
     }
   }
 }
