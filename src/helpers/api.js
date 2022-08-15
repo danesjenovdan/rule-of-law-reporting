6c3eaed859ef60c8c20daf564d1d77269c8ddeea
LepkoQQ <lepko.san@gmail.com>
2022-08-15 21:32:35 +0200

    search contrib description as well

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -85,7 +85,9 @@ export async function getContributions(
     filter.Objavljeno = true;
   }
   if (search) {
-    filter['Ime prispevka'] = { op: 'like', value: search };
+    filter.or = filter.or || {};
+    filter.or['Ime prispevka'] = { op: 'like', value: search };
+    filter.or['O področju prispevka'] = { op: 'like', value: search };
   }
   const filterUsers = {};
   const userId = localStorage.getItem('user_id');
